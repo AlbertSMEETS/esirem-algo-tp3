8b4f7c7da24b73cd562825ab2ea1a6315a4f25e4
AlbertSMEETS <albert.smeets@laposte.net>
2023-11-09 14:39:00 +0100

    Creation de la structure equation.

--- main.c
+++ main.c
@@ -2,6 +2,19 @@
 
 enum SolutionType {NONE = 0, ONE = 1, TWO = 2};
 
+struct Solution {
+    enum SolutionType type;
+    double x0;
+    double x1;
+    double x2;
+};
+
+struct Equation{
+    int a,b,c;
+    struct Solution solution;
+
+};
+
 int main() {
     return 0;
 }
