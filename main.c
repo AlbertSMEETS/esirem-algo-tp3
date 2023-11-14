34754c436a1abcca5b8d2030ee482f1f1c8d19af
AlbertSMEETS <albert.smeets@laposte.net>
2023-11-14 10:41:51 +0100

    Ajout du 1.3 en code c, et rectification des conditions des if dans show solution.

--- main.c
+++ main.c
@@ -16,18 +16,37 @@ struct Equation{
 };
 
 void ShowSolution(struct Solution sol){
-    if(sol.type = 0){
+    if(sol.type == 0){
         printf("Il n'y a aucune solution.\n");
     }
 
-    if(sol.type = 1){
+    if(sol.type == 1){
         printf("La solution simple est x0 =%f.\n", sol.x0);
     }
 
-    if(sol.type = 2){
+    if(sol.type == 2){
         printf("Les deux solution sont %f et %f.\n", sol.x1, sol.x2);
     }
 }
+
+void test(){
+    struct Solution sol1,sol2,sol3,sol4,sol5,sol6;
+    int i = 0;
+
+    struct Solution solution[i];
+    solution[0]=sol1;
+    solution[1]=sol2;
+    solution[2]=sol3;
+    solution[3]=sol4;
+    solution[4]=sol5;
+    solution[5]=sol6;
+
+    for(i=0;i<=sizeof(solution);i++){
+        ShowSolution(solution[i]);
+    }
+}
+
+
 
 int main() {
     return 0;
