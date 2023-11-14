bd77d2849b4fe265babaa33c15a53cbe21ea4f13
AlbertSMEETS <albert.smeets@laposte.net>
2023-11-14 12:39:19 +0100

    Suite du code, ajout de 1.4 et 1.5 en c.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 enum SolutionType {NONE = 0, ONE = 1, TWO = 2};
 
@@ -46,7 +47,31 @@ void test(){
     }
 }
 
+struct Solution resolveOne(double a, double b){
+    struct Solution sol;
+    sol.type = 1;
+    sol.x0= -(b/a);
+    sol.x1 = 0;
+    sol.x2 = 0;
+    return sol;
+}
 
+struct Solution resolveTwo(double a, double b, double c){
+    struct Solution sol;
+    if(b*b-(4*a*c)>0){
+        sol.type = 2;
+        sol.x0 = 0;
+        sol.x1 = (-b-sqrt(b*b-(4*a*c)))/2*a;
+        sol.x2 = (-b+sqrt(b*b-(4*a*c)))/2*a;
+    }
+    if(b*b-(4*a*c)==0){
+        sol.type = 1;
+        sol.x0 = -b/(2*a);
+        sol.x1 = 0;
+        sol.x2 = 0;
+    }
+    return sol;
+}
 
 int main() {
     return 0;
