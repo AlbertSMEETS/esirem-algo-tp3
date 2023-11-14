95fc8d6a76a645655199e5ef07b8e747b593cc00
AlbertSMEETS <albert.smeets@laposte.net>
2023-11-14 13:15:17 +0100

    Finalisation de la fonction centrale 1.6, debut de la 1.7

--- main.c
+++ main.c
@@ -47,7 +47,7 @@ void test(){
     }
 }
 
-struct Solution resolveOne(double a, double b){
+struct Solution resolveOne(int a, int b){
     struct Solution sol;
     sol.type = 1;
     sol.x0= -(b/a);
@@ -56,7 +56,7 @@ struct Solution resolveOne(double a, double b){
     return sol;
 }
 
-struct Solution resolveTwo(double a, double b, double c){
+struct Solution resolveTwo(int a, int b, int c){
     struct Solution sol;
     if(b*b-(4*a*c)>0){
         sol.type = 2;
@@ -72,6 +72,17 @@ struct Solution resolveTwo(double a, double b, double c){
     }
     return sol;
 }
+
+struct Solution resolve(int a, int b, int c){
+    if(a==0){
+        return resolveOne(b,c);
+    }
+    else{
+        return resolveTwo(a,b,c);
+    }
+}
+
+
 
 int main() {
     return 0;
